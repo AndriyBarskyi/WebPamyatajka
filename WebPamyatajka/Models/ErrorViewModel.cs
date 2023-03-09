1cce19a4bf7b06c4c4af14d221ba2cc2573e4983
AndriyBarskyi <39509993+AndriyBarskyi@users.noreply.github.com>
2023-03-09 21:24:09 +0200

    create initial project

---
+++ WebPamyatajka/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebPamyatajka.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
